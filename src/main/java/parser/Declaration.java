71361540d673ee4681eb6874b6d56c1baed0aaeb
michelleamg <amateog1700@alumno.ipn.mx>
2023-12-17 20:58:50 -0600

    Declaraciones

---
+++ src/main/java/parser/Declaration.java
@@ -0,0 +1,31 @@
+package parser;
+
+public class Declaration {
+
+    void declaration(){
+        if () return;
+        if(){
+            //DECLARATION -> FUN_DECL DECLARATION
+        }else if{
+            //DECLARATION ->VAR_DECL DECLARATION
+        }else if {
+            //DECLARATION -> STATEMENT DECLARATION
+        }else if {
+            //DECLARATION ->vacio
+        }
+
+    }
+    void func_decl(){
+        //FUN_DECL -> fun FUNCTION
+    }
+    void var_decl() {
+        //VAR_DECL -> var id VAR_INIT ;
+    }
+
+    void var_init(){
+        //VAR_INIT -> = EXPRESSION
+        //-> Ɛ
+    }
+
+
+}
