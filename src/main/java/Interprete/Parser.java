202731517ef13ed0685a156ef498481519674dff
TheTortillas <sebasverac331@gmail.com>
2023-12-20 17:55:04 -0600

    Comienzo funcion de error

--- src/main/java/Interprete/Parser.java
+++ src/main/java/Interprete/Parser.java
@@ -58,6 +58,7 @@ public class Parser {
     //FIN DE CADENA
     private final Token EOF = new Token(TipoToken.EOF, "",i);
 
+    // Modificar para usar la funcion de error de interprete
     private boolean hayErrores = false; //VAR. de errores
     public Parser(List<Token> tokens){
         this.tokens = tokens;
