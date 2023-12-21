683bd6971257f3011986f5c59eea980b51a3d78c
michelleamg <amateog1700@alumno.ipn.mx>
2023-12-21 14:09:01 -0600

    Pruebas ASSIGN Y ARBOL AST

--- src/main/java/Interprete/Arbol.java
+++ src/main/java/Interprete/Arbol.java
@@ -1,19 +1,9 @@
 package Interprete;
-import java
-import java.time.temporal.ValueRange;
-import java.util.ArrayList;
-import java.util.List;
-import.* inteprete;
 public class Arbol {
     private final Nodo raiz;
 
     public Arbol(Nodo raiz){
         this.raiz = raiz;
-        for(Nodo n : raiz.getHijos()){
-            Token t = n.getValue();
-            switch (t.tipo)
-            {
-
     }
 
     public void recorrer(){
@@ -22,51 +12,9 @@ public class Arbol {
 
     private void recorrerAux(Nodo nodo, int nivel) {
         if (nodo == null) return;
-        for( nodo: raiz.getHijos()){
-            Token nodo = n.getValue();
-            switch (t.tipo) {
-
-                case PLUS:
-                case MINUS:
-                case STAR:
-                case SLASH:
-                case LESS:
-                case LESS_EQUAL:
-                case GREATER_EQUAL:
-                case GREATER:
-                case EQUAL_EQUAL:
-                case BANG_EQUAL:
-                case AND:
-                case OR:
-                    SolverAritmetico solver = new SolverAritmetico(n);
-                    solver.solve();
-                    break;
-
-                case VAR:
-                    VarSolver varSolver = new VarSolver(n);
-                    varSolver.solve();
-                    break;
-                case IF:
-                    IfSolver ifSolver = new IfSolver(n);
-                    ifSolver.solve();
-                    break;
-                case FOR:
-                    ForSolver forSolver = new ForSolver(n);
-                    forSolver.solve();
-                    break;
-                case WHILE:
-                    WhileSolver whileSolver = new WhileSolver(n);
-                    whileSolver.solve();
-                    break;
-                case PRINT:
-                    PrintSolver printSolver = new PrintSolver(n);
-                    printSolver.solve();
-                    break;
-            }
-
-        }
 
         // Aquí puedes procesar el nodo, por ejemplo, imprimir su valor
+
         imprimirNodo(nodo, nivel);
 
         // Si el nodo tiene hijos, recorrer cada uno de ellos
@@ -82,10 +30,10 @@ public class Arbol {
         String prefijo = new String(new char[nivel]).replace("\0", "  ");
 
         // Obtener el valor del token en el nodo
-        Token t = nodo.getValue();
+        Token t = nodo
 
         // Imprimir el token con la indentación apropiada
         System.out.println(prefijo + "Token: " + t.getLexema() + " (Tipo: " + t.getTipo() + ")");
     }
 }
-
+.getValue();
